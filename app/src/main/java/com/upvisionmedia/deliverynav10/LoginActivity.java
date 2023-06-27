13246b15868a1e6a797edbfc519955a6c8a2ed64
iamalexstanciu <alexstanciu@outlook.it>
2023-06-27 17:01:03 +0300

    done login, register and homepage, working now on database connection

---
+++ app/src/main/java/com/upvisionmedia/deliverynav10/LoginActivity.java
@@ -0,0 +1,77 @@
+package com.upvisionmedia.deliverynav10;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class LoginActivity extends Activity {
+
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
+
+    // Update the following variables with your PostgreSQL database credentials
+    private static final String DB_URL = "jdbc:postgresql://db.xapysolltgbcysltbuxe.supabase.co:5432/postgres";
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "Mister92skulls!";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = emailEditText.getText().toString().trim();
+                String password = passwordEditText.getText().toString().trim();
+
+                // Perform login logic here
+                if (isValidCredentials(email, password)) {
+                    Toast.makeText(LoginActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+
+                    // Start the MainActivity or any other desired activity
+                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Invalid credentials", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private boolean isValidCredentials(String email, String password) {
+        try {
+            Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            String query = "SELECT COUNT(*) FROM users WHERE email = ? AND password = ?";
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setString(1, email);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            resultSet.next();
+            int count = resultSet.getInt(1);
+            statement.close();
+            connection.close();
+
+            return count > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
--- app/src/main/java/com/upvisionmedia/deliverynav10/RegistrationActivity.java
+++ app/src/main/java/com/upvisionmedia/deliverynav10/RegistrationActivity.java
@@ -2,50 +2,100 @@ package com.upvisionmedia.deliverynav10;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class RegistrationActivity extends Activity {
 
-    private EditText usernameEditText;
+    private EditText emailEditText;
+    private EditText nameEditText;
     private EditText passwordEditText;
+    private EditText confirmPasswordEditText;
     private Button registerButton;
+
+    // Update the following variables with your PostgreSQL database credentials
+    private static final String DB_URL = "jdbc:postgresql://db.xapysolltgbcysltbuxe.supabase.co:5432/postgres";
+    private static final String DB_USERNAME = "postgres";
+    private static final String DB_PASSWORD = "Mister92skulls!";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        usernameEditText = findViewById(R.id.usernameEditText);
+        emailEditText = findViewById(R.id.emailEditText);
+        nameEditText = findViewById(R.id.nameEditText);
         passwordEditText = findViewById(R.id.passwordEditText);
+        confirmPasswordEditText = findViewById(R.id.confirmPasswordEditText);
         registerButton = findViewById(R.id.registerButton);
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String username = usernameEditText.getText().toString().trim();
+                String email = emailEditText.getText().toString().trim();
+                String name = nameEditText.getText().toString().trim();
                 String password = passwordEditText.getText().toString().trim();
+                String confirmPassword = confirmPasswordEditText.getText().toString().trim();
 
-                if (registerUser(username, password)) {
-                    Toast.makeText(RegistrationActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
-                    setResult(RESULT_OK);
-                } else {
-                    Toast.makeText(RegistrationActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
-                    setResult(RESULT_CANCELED);
+                if (!password.equals(confirmPassword)) {
+                    Toast.makeText(RegistrationActivity.this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
-                finish();
+                new RegisterUserTask().execute(email, name, password);
             }
         });
     }
 
-    private boolean registerUser(String username, String password) {
-        // Implement your registration logic here
-        // Return true if registration is successful, false otherwise
-        // You can replace this with your own implementation, such as checking against a database or API
-        return true;
+    private class RegisterUserTask extends AsyncTask<String, Void, Boolean> {
+        @Override
+        protected Boolean doInBackground(String... params) {
+            String email = params[0];
+            String name = params[1];
+            String password = params[2];
+
+            try {
+                Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+                String query = "INSERT INTO users (email, name, password) VALUES (?, ?, ?)";
+                PreparedStatement statement = connection.prepareStatement(query);
+                statement.setString(1, email);
+                statement.setString(2, name);
+                statement.setString(3, password);
+                int rowsAffected = statement.executeUpdate();
+                statement.close();
+                connection.close();
+
+                return rowsAffected > 0;
+            } catch (SQLException e) {
+                e.printStackTrace();
+                return false;
+            }
+        }
+
+        @Override
+        protected void onPostExecute(Boolean success) {
+            if (success) {
+                Toast.makeText(RegistrationActivity.this, "Registration successful", Toast.LENGTH_SHORT).show();
+                setResult(RESULT_OK);
+
+                // Start the Login activity
+                Intent intent = new Intent(RegistrationActivity.this, LoginActivity.class);
+                startActivity(intent);
+            } else {
+                Toast.makeText(RegistrationActivity.this, "Registration failed", Toast.LENGTH_SHORT).show();
+                setResult(RESULT_CANCELED);
+            }
+
+            finish();
+        }
     }
 }
