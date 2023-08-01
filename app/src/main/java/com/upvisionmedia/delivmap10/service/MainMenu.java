18c2872f7d0b3c7cbd17209a55057d695a6c22ce
iamalexstanciu <alexstanciu@outlook.it>
2023-08-01 11:10:00 +0300

    Added new background.

--- app/src/main/java/com/upvisionmedia/delivmap10/service/MainMenu.java
+++ app/src/main/java/com/upvisionmedia/delivmap10/service/MainMenu.java
@@ -1,19 +1,26 @@
 package com.upvisionmedia.delivmap10.service;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toolbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.upvisionmedia.delivmap10.R;
 
 public class MainMenu extends AppCompatActivity {
 
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_menu);
+
     }
 
 }
