a2c3cd6c6bf3402779dce816a76a644590704a1d
iamalexstanciu <alexstanciu@outlook.it>
2023-08-08 13:27:11 +0300

    Started setting activity layout 1.1.

--- app/src/main/java/com/upvisionmedia/delivmap10/homepage/SettingsActivity.java
+++ app/src/main/java/com/upvisionmedia/delivmap10/homepage/SettingsActivity.java
@@ -1,17 +1,72 @@
 package com.upvisionmedia.delivmap10.homepage;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.upvisionmedia.delivmap10.R;
+import com.upvisionmedia.delivmap10.pages.fragments.DelivFragment;
+import com.upvisionmedia.delivmap10.pages.fragments.MessagesFragment;
+import com.upvisionmedia.delivmap10.pages.fragments.ProfileFragment;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    FloatingActionButton floatingActionButton;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
+
+        // Bottom app bar functionality
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        floatingActionButton =  findViewById(R.id.fab);
+
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.nav_home) {
+                // Handle home button click
+                Intent intent = new Intent(this, MainMenu.class);
+                startActivity(intent);
+                return true;
+            } else if (item.getItemId() == R.id.nav_message) {
+                // Handle search button click
+                replaceFragment(new MessagesFragment()); // Replace with the appropriate fragment
+                return true;
+            } else if (item.getItemId() == R.id.nav_settings) {
+                // Handle profile button click
+                Intent intent = new Intent(this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            }
+
+            else if (item.getItemId() == R.id.nav_profile) {
+
+                replaceFragment(new ProfileFragment()); // Replace with the appropriate fragment
+                return true;
+            }
+            return false;
+        });
+
+        // Floating button functionality
+
+        floatingActionButton.setOnClickListener(v->{
+            replaceFragment(new DelivFragment());
+        });
+
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container_main_menu, fragment)
+                .addToBackStack(null)
+                .commit();
     }
 }
